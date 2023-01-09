67fb8a047574ec988d68486a03e715db7e352fbd
margareth-mee <margoifma@hotmail.com>
2023-01-09 15:49:12 -0300

    adicionando beach model com mongoose

--- test/functional/beaches.test.ts
+++ test/functional/beaches.test.ts
@@ -1,5 +1,8 @@
+import { Beach } from '@src/models/beach';
+
 describe('Beaches functional tests', () => {
   describe('When creating a beach', () => {
+    beforeAll(async () => await Beach.deleteMany({}));
     it('should create a beach with success', async () => {
       const newBeach = {
         lat: -33.792726,
@@ -11,6 +14,22 @@ describe('Beaches functional tests', () => {
       const response = await global.testRequest.post('/beaches').send(newBeach);
       expect(response.status).toBe(201);
       expect(response.body).toEqual(expect.objectContaining(newBeach));
+    });
+
+    it('should return 422 when there is a validation error', async () => {
+      const newBeach = {
+        lat: 'invalid_string',
+        lng: 151.289824,
+        name: 'Manly',
+        position: 'E',
+      };
+      const response = await global.testRequest.post('/beaches').send(newBeach);
+
+      expect(response.status).toBe(422);
+      expect(response.body).toEqual({
+        error:
+          'Beach validation failed: lat: Cast to Number failed for value "invalid_string" (type string) at path "lat"',
+      });
     });
   });
 });
