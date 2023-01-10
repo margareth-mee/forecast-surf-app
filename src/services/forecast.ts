f2ea1ccdbb52b53e85b62145344ce944035a80e3
margareth-mee <margoifma@hotmail.com>
2023-01-10 14:40:16 -0300

    integrando beach e forecast | mockando API's com nock

--- src/services/forecast.ts
+++ src/services/forecast.ts
@@ -1,21 +1,7 @@
 import { InternalError } from './../util/errors/internal-error';
 import { ForecastPoint } from './../clients/stormGlass';
 import { StormGlass } from '@src/clients/stormGlass';
-
-export enum BeachPosition {
-  S = 'S',
-  E = 'E',
-  W = 'W',
-  N = 'N',
-}
-
-export interface Beach {
-  name: string;
-  position: BeachPosition;
-  lat: number;
-  lng: number;
-  user: string;
-}
+import { Beach } from '@src/models/beach';
 
 export interface TimeForecast {
   time: string;
